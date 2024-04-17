051360fb6ed83d5c94a28715cfdd41c383720f2a
Meshram2004 <157383058+Meshram2004@users.noreply.github.com>
2024-04-17 18:58:08 +0530

    Research and Innovation park included

---
+++ rni.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rni" tilewidth="64" tileheight="64" tilecount="629" columns="37">
+ <image source="rni.jpg" width="2400" height="1124"/>
+</tileset>
