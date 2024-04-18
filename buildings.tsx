945f4e10052c239b8e2fdddb094f6eba3cb84346
Meshram2004 <157383058+Meshram2004@users.noreply.github.com>
2024-04-18 23:05:33 +0530

    loading_map_testing

---
+++ buildings.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="buildings " tilewidth="64" tileheight="64" tilecount="36" columns="6">
+ <image source="../OneDrive/Documents/Tiled_project/IIT_Delhi_map/buildings .png" width="400" height="400"/>
+</tileset>
